99fdd3680f8f886dbaf10d96efbc0ae28c88f854
v.luchko <luchko@ittest-team.ru>
2025-05-23 14:02:07 +0300

    Added image saving

--- app/main.go
+++ app/main.go
@@ -4,9 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
-    "strconv"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
 
 	"github.com/go-sql-driver/mysql"
 )
@@ -15,37 +20,10 @@ type NewsItem struct {
     Id int
     Title string
     Body string
+    Image sql.NullString
 }
 
 var db *sql.DB;
-
-var newsList[]NewsItem;
-
-
-func loadNewsList() error {
-    rows, err := db.Query("SELECT * FROM news");
-
-    if (err != nil) {
-        return err
-    }
-    defer rows.Close()
-
-    for rows.Next() {
-        var newsItem NewsItem
-
-        if err := rows.Scan(&newsItem.Id, &newsItem.Title, &newsItem.Body); err != nil {
-            return  err
-        }
-
-        newsList = append(newsList, newsItem)
-    }
-
-    if err := rows.Err(); err != nil {
-        return err
-    }
-
-    return nil
-}
 
 func getNewsById(id int) (*NewsItem, error) {
     row, err := db.Query("SELECT * FROM news WHERE id = ?", id)
@@ -57,7 +35,7 @@ func getNewsById(id int) (*NewsItem, error) {
     var newsItem NewsItem
 
     row.Next()
-    if err := row.Scan(&newsItem.Id, &newsItem.Title, &newsItem.Body); err != nil {
+    if err := row.Scan(&newsItem.Id, &newsItem.Title, &newsItem.Body, &newsItem.Image); err != nil {
         return nil, err
     }
 
@@ -69,6 +47,32 @@ func getNewsById(id int) (*NewsItem, error) {
 }
 
 func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+    var newsList[]NewsItem;
+
+    rows, err := db.Query("SELECT * FROM news");
+
+    if (err != nil) {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return;
+    }
+    defer rows.Close() 
+
+    for rows.Next() {
+        var newsItem NewsItem
+
+        if err := rows.Scan(&newsItem.Id, &newsItem.Title, &newsItem.Body, &newsItem.Image); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return;
+        }
+
+        newsList = append(newsList, newsItem)
+    }
+
+    if err := rows.Err(); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return;
+    }
+
     tmpl := template.Must(template.ParseFiles("./templates/index.html"))
 
     tmpl.Execute(w, newsList)
@@ -119,7 +123,7 @@ func updatePageHandler(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return;
     }
-
+    
     _, err = db.Exec("UPDATE news SET title = ?, body = ? WHERE id = ?", r.FormValue("title"), r.FormValue("body"), newsId)
 
     if (err != nil) {
@@ -127,10 +131,65 @@ func updatePageHandler(w http.ResponseWriter, r *http.Request) {
         return;
     }
 
+    file,header,_ :=r.FormFile("image")
+
+    if  file != nil {
+
+        savedImage, err := saveImage(file, header)
+
+        if (err != nil) {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        var currentImage string;
+        db.QueryRow("SELECT image FROM news where id = ?", newsId).Scan(&currentImage)
+
+        fmt.Print("Current image:" + currentImage);
+
+        _, err = db.Exec("UPDATE news SET image = ? WHERE id = ?", savedImage, newsId);
+
+        if (err != nil) {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return;
+        }
+
+        if currentImage != "" {
+            os.Remove("./images/" + currentImage)
+        }
+    }
+   
+
     http.Redirect(w, r, "/detail/" + strconv.Itoa(newsId), http.StatusFound)
 }
 
+func saveImage(f multipart.File, h *multipart.FileHeader) (*string, error) {
+    defer f.Close()
+
+    path := filepath.Join(".", "images")
+
+
+    fullPath := path + "/" + h.Filename;
+
+    fd, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+
+    if err != nil {
+        return nil, err;
+    }
+
+    defer fd.Close()
+
+    _, err = io.Copy(fd, f)
+    
+    if err != nil {
+        return nil, err;
+    }
+
+    return &h.Filename, nil
+}
+
 func createPageHandler(w http.ResponseWriter, r *http.Request) {
+
     result, err := db.Exec("INSERT INTO news (title, body) VALUES (?, ?)", r.FormValue("title"), r.FormValue("body"))
 
     if (err != nil) {
@@ -143,6 +202,25 @@ func createPageHandler(w http.ResponseWriter, r *http.Request) {
     if (err != nil) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return;
+    }
+
+    file,header,_ :=r.FormFile("image")
+
+    if  file != nil {
+
+        savedImage, err := saveImage(file, header)
+
+        if (err != nil) {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        _, err = db.Exec("UPDATE news SET image = ? WHERE id = ?", savedImage, insertedId);
+
+        if (err != nil) {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return;
+        }
     }
 
     http.Redirect(w, r, "/detail/" + strconv.Itoa(int(insertedId)), http.StatusFound)
@@ -178,11 +256,9 @@ func main() {
         log.Fatal(pingErr)
     }
     fmt.Println("Connected!")
-
-    err = loadNewsList();
-
-    if (err != nil) {
-        log.Fatal(err)
+    rootdir, err := os.Getwd()
+    if err != nil {
+        panic(err)
     }
 
     http.HandleFunc("/", indexPageHandler)
@@ -191,6 +267,7 @@ func main() {
     http.HandleFunc("/update/", updatePageHandler)
     http.HandleFunc("/new/", creationPageHandler)
     http.HandleFunc("/create/", createPageHandler)
+    http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir(path.Join(rootdir, "images/")))))
 
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
